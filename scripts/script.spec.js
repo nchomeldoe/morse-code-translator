1afaa78fd9af186ab4c5ba2e6ae0a57f4d5e946a
Nathalia Robbins-Cherry <nchomeldoe@gmail.com>
2022-07-11 13:53:14 +0100

    wrote a series of tests for english to morse

--- scripts/script.spec.js
+++ scripts/script.spec.js
@@ -4,7 +4,26 @@ import { translateEnglishToMorse } from "./script";
 describe("english to morse code translator", () => {
   it("should translate 'hello' to '.... . .-.. .-.. ---'", () => {
     const result = translateEnglishToMorse("hello");
-    // expect(result).toBe(".... . .-.. .-.. ---");
-    expect(result).toBe("working");
+    expect(result).toBe(".... . .-.. .-.. ---");
+  });
+  it("should insert spaces between letters and forward slashes between words", () => {
+    const result = translateEnglishToMorse("hello world");
+    expect(result).toBe(".... . .-.. .-.. --- / .-- --- .-. .-.. -..");
+  });
+  it("should translate capital letters the same as lower case ones", () => {
+    const result = translateEnglishToMorse("My name is Nathalia");
+    expect(result).toBe(
+      "-- -.-- / -. .- -- . / .. ... / -. .- - .... .- .-.. .. .-",
+    );
+  });
+  it("should translate numbers, full stops and question marks", () => {
+    const result = translateEnglishToMorse("How old are you? 33.");
+    expect(result).toBe(
+      ".... --- .-- / --- .-.. -.. / .- .-. . / -.-- --- ..- ..--.. / ...-- ...-- .-.-.-",
+    );
+  });
+  it("should return '[symbol] unavailable' if a symbol that is not in the disctionary is entered", () => {
+    const result = translateEnglishToMorse("This is a test:");
+    expect(result).toContain("':' unavailable");
   });
 });
